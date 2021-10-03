6998b1c6b809a3c7e7f23a8cf2bf93ba2b088448
SKKR <sanjaykumar.kkr@microfocus.com>
2021-10-03 14:03:09 +0530

    1.0.4

--- logger.go
+++ logger.go
@@ -2,16 +2,21 @@ package logger
 
 import (
 	"net/http"
-	"strings"
 )
 
-func (l Logger) Log() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		p := strings.Split(r.URL.Path, "/")
-		rw.Header().Set("Content-Type", "application/json")
-		rw.Write([]byte(p[1]))
+var message string
+
+func (l Logger) Log(logMessage string) {
+	message = message + "\n" + string(logMessage)
+}
+
+func (l Logger) Show() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Set("Content-Type", "text/plain")
+		rw.Write([]byte(message))
 	})
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", mux)
 }
 
 type Logger struct {
