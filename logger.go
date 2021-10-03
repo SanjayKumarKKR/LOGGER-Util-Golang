5d53304c4dfc2ecf8419773fbc00d438e58056c1
SKKR <sanjaykumar.kkr@microfocus.com>
2021-10-03 12:11:46 +0530

    sas

--- logger.go
+++ logger.go
@@ -5,16 +5,18 @@ import (
 	"strings"
 )
 
-func log() {
+func (l logger) log() {
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		p := strings.Split(r.URL.Path, "/")
 		rw.Header().Set("Content-Type", "application/json")
 		rw.Write([]byte(p[1]))
-
 	})
 	http.ListenAndServe(":9090", nil)
 }
 
 type World struct {
 	Message string `json:"message"`
+}
+
+type logger struct {
 }
