35c18589b5418f914d93d3689df631126d31edcc
SKKR <sanjaykumar.kkr@microfocus.com>
2021-10-03 11:49:44 +0530

    logger init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SanjayKumarKKR/LOGGER-Util-Golang
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,20 @@
+package logger
+
+import (
+	"net/http"
+	"strings"
+)
+
+func logger() {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		p := strings.Split(r.URL.Path, "/")
+		rw.Header().Set("Content-Type", "application/json")
+		rw.Write([]byte(p[1]))
+
+	})
+	http.ListenAndServe(":9090", nil)
+}
+
+type World struct {
+	Message string `json:"message"`
+}
