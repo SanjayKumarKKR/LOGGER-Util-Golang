452f392284d6b5374e7aa7339e482add14156c72
SKKR <sanjaykumar.kkr@microfocus.com>
2021-10-12 12:14:31 +0530

    Logger: changes for v1.0.10

--- logger.go
+++ logger.go
@@ -7,17 +7,23 @@ import (
 var message string
 
 func (l Logger) Log(logMessage string) {
-	message = message + "\n" + string(logMessage)
+	if l.enabled {
+		message = message + "\n" + string(logMessage)
+	}
 }
 
 func (l Logger) Show() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		rw.Header().Set("Content-Type", "text/plain")
-		rw.Write([]byte(message))
-	})
-	http.ListenAndServe(":8080", mux)
+	if l.enabled {
+		mux := http.NewServeMux()
+		mux.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+			rw.Header().Set("Content-Type", "text/plain")
+			rw.Write([]byte(message))
+		})
+		http.ListenAndServe(":"+l.port, mux)
+	}
 }
 
 type Logger struct {
+	port    string
+	enabled bool
 }
